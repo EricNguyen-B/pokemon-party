5f42fbfa64367baa0188004cf73bdab875c64dac
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-09-16 23:46:20 -0400

    Add pokemon details for listings

--- party-app/src/app/pokemons/pokemons.component.ts
+++ party-app/src/app/pokemons/pokemons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+import { Pokemon } from '../../pokemon';
 
 @Component({
   selector: 'app-pokemons',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemons.component.css']
 })
 export class PokemonsComponent implements OnInit {
+
+  pokemon : Pokemon = {
+    id: 1,
+    type: 'fire',
+    level: 14,
+    name: 'Charmander'
+  };
 
   constructor() { }
 
---
+++ party-app/src/pokemon.ts
@@ -0,0 +1,6 @@
+export interface Pokemon {
+    id: number,
+    type: string,
+    level: number, 
+    name: string
+}
